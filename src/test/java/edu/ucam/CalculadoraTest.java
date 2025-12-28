c464f3fe0c5bbf77f9595b42adfbc265f519593a
Juan Jose Arroniz Martinez <jjarroniz@alu.ucam.edu>
2025-12-28 21:07:47 +0100

    Forzar fallo para pruebas de Jenkins

--- src/test/java/edu/ucam/CalculadoraTest.java
+++ src/test/java/edu/ucam/CalculadoraTest.java
@@ -8,6 +8,6 @@ public class CalculadoraTest {
     @Test
     public void testSumar() {
         Calculadora calc = new Calculadora();
-        assertEquals(5, calc.sumar(2, 2));
+        assertEquals(5, calc.sumar(3, 2));
     }
 }
